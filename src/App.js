2dc0110df5a1efdc3d412138fddd3b22cacb62a1
Zaki Gouizi <zaki.gouizi@student.junia.com>
2022-03-26 23:07:49 +0100

    start implementing Leagues

--- src/App.js
+++ src/App.js
@@ -16,14 +16,29 @@ import {
 } from '@chakra-ui/react';
 import Header from './components/header.jsx';
 import FootballClub from './components/football_club.jsx';
-
+import League from './components/league.jsx';
 
 function App() {
+  state = {
+    // 39 Premier League | 180 Championship
+    // 61 Ligue 1 | 62 Ligue 2
+    // 140 La Liga
+    //135 Serie A
+    //78 Bundesliga 1 | 79 Bundesliga 2
+    //144 Jupiler Pro League
+    // 197 Super League
+    // 203 Toto Super Lig
+    // 88 Euredivise
+    leagues : [39, 180, 61, 62, 140, 135, 78, 79, 144, 197, 203, 88]
+  };
   return (
     <ChakraProvider theme={theme} >
       <Header/>
       <Flex>
-        <FootballClub/>
+        {
+        //<FootballClub club_id={'33'} selected={true} />
+        }
+        <League/>
       </Flex>
     </ChakraProvider>
   );
